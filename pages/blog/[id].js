f448c112c89c95a37b542f073cf6c3ae1c091684
Thanh NC <thanh-nc@dgw.asia.com>
2023-10-26 09:42:07 +0700

    3rd commit

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,6 +1,12 @@
-
+import Link from "next/link";
 import { client } from "../../libs/client";
 import styles from '../../styles/Home.module.scss';
+import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const getStaticProps = async (context) => {
     const id = context.params.id;
@@ -24,11 +30,18 @@ export const getStaticPaths = async () => {
 
 export default function BlogId({blog}) {
     return (
-        <main className={styles.main}>
-            <h1 className={styles.title}>{blog.title}</h1>
-            <p className={styles.publishedAt}>{blog.publishedAt}</p>
-            <p>{blog.category && blog.category.name}</p>
-            <div dangerouslySetInnerHTML={{__html: `${blog.body}`}} className={styles.post}></div>
-        </main>
+        <>
+            <header className={styles.header}>
+                <div className={styles.headerInner}>
+                    <Link href="/">＜ 戻る</Link>
+                </div>
+            </header>
+            <main className={styles.main}>
+                <h1 className={styles.title}>{blog.title}</h1>
+                <p className={styles.publishedAt}>{dayjs.utc(blog.publishedAt).tz('Asia/Tokyo').format('YYYY-MM-DD HH:MM')}</p>
+                <p>カテゴリ: {blog.category && blog.category.name}</p>
+                <div dangerouslySetInnerHTML={{__html: `${blog.body}`}} className={styles.post}></div>
+            </main>
+        </>
     )
 };
